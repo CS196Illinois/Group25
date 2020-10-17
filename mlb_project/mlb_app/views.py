77b87c8152c2d58d29a5f436de96bd9bff296301
carterwsmith <csmithonthetrack@gmail.com>
2020-10-17 18:38:18 -0500

    retrieve and display upcoming games

--- mlb_project/mlb_app/views.py
+++ mlb_project/mlb_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+import mlbgame, datetime
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    now = datetime.datetime.now()
+
+    #retrieve today's games
+    games = mlbgame.day(now.year, now.month, now.day)[0:10]
+    if games is []:
+        #go to next season
+        date = mlbgame.important_dates(now.year+1).first_date_seas.split('-')
+        year = int(date[0])
+        month = int(date[1])
+        day = int(date[2][:2])
+        mlbgame.day(year, month, day)[0:10]
+
+    return render(request, 'home.html', {'data': games})
