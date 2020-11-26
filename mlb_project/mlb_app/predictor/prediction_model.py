72f72ccf3a57fe198e527d0b0ad06a7798844357
Ayan Mallik <ayan.mallik20@gmail.com>
2020-11-26 12:15:17 -0600

    Set up function that calls API and returns predicted winner

--- mlb_project/mlb_app/predictor/prediction_model.py
+++ mlb_project/mlb_app/predictor/prediction_model.py
@@ -6,12 +6,16 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 ##call the API to get stats for both teams needed to predict game
-#def get_data(home_team_name, away_team_name):
+def get_winner(home_team_name, away_team_name):
 #   code to get stats from API
-#   return predict_game(stats)
+   prediction = predict_game(stats)
 
-def predict_game(home_delta_ERA, home_delta_RBI, home_delta_SLG, home_delta_AVG,
-                away_delta_ERA, away_delta_RBI, away_delta_SLG, away_delta_AVG):
+   if (prediction == 1):
+      return home_team_name
+   else:
+      return away_team_name
+
+def predict_game(delta_ERA, delta_RBI, delta_SLG, delta_AVG):
     # Creates path to models
     path = os.path.join(settings.MODELS, 'models.p')
  
@@ -19,14 +23,9 @@ def predict_game(home_delta_ERA, home_delta_RBI, home_delta_SLG, home_delta_AVG,
     with open(path, 'rb') as pickled:
        data = pickle.load(pickled)
     logreg = data['regression']
-    
-    diff_ERA = home_delta_ERA - away_delta_ERA
-    diff_RBI = home_delta_RBI - away_delta_RBI
-    diff_SLG = home_delta_SLG - away_delta_SLG
-    diff_AVG = home_delta_AVG - away_delta_AVG
 
     # Stores the team stats in a numpy array and predicts the outcome of the game
-    team_stats = np.array([diff_ERA, diff_RBI, diff_SLG, diff_AVG]).reshape(1, 4)
+    team_stats = np.array([delta_ERA, delta_RBI, delta_SLG, delta_AVG]).reshape(1, 4)
     prediction = logreg.predict(team_stats)
 
-    return prediction
+    return prediction[0]
